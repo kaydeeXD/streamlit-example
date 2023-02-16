39d3fce7aedf910fc5107c798ea186ac9594e319
Amrit Kumar Dash <95899268+kaydeeXD@users.noreply.github.com>
2023-02-16 18:11:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,35 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from backtrader_plotting import Bokeh
 
-"""
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Import your Backtrader strategy
+from my_strategy import MyStrategy
 
+# Define the Streamlit app
+def main():
+    # Set the app title
+    st.set_page_config(page_title='Backtrader Strategy', page_icon=':chart_with_upwards_trend:')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # Set the app header
+    st.header('Backtrader Strategy')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # Define the sidebar
+    st.sidebar.header('Settings')
+    ticker = st.sidebar.text_input('Enter Ticker Symbol', 'AAPL')
+    start_date = st.sidebar.date_input('Start Date')
+    end_date = st.sidebar.date_input('End Date')
 
-    points_per_turn = total_points / num_turns
+    # Create a Bokeh plot for displaying the strategy
+    bokeh_plot = Bokeh(style='bar', plot_mode='single')
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    # Run the strategy with the given settings
+    cerebro = bt.Cerebro()
+    data = bt.feeds.YahooFinanceData(dataname=ticker, fromdate=start_date, todate=end_date)
+    cerebro.adddata(data)
+    cerebro.addstrategy(MyStrategy)
+    cerebro.run()
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # Plot the strategy using the Bokeh plot
+    bokeh_plot.plot(cerebro)
+
+if __name__ == '__main__':
+    main()
